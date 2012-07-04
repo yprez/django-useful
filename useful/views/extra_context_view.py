d06cb961b601cf0e5a5d7ff0945a94f2f4e17a24
Yuri Prezument <y@yprez.com>
2012-07-05 00:16:21 +0300

    Add docstring

--- useful/views/extra_context_view.py
+++ useful/views/extra_context_view.py
@@ -2,6 +2,9 @@ from django.views.generic import TemplateView
 
 
 class ExtraContextTemplateView(TemplateView):
+    """
+    Extension of Django TemplateView CBV that allows adding extra context
+    """
     extra_context = None
 
     def get_context_data(self, *args, **kwargs):

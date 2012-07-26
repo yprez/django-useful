9f90a73fa43b4ee79687410d1b886efca7eb434b
Yuri Prezument <y@yprez.com>
2012-07-26 21:53:27 +0300

    More tests for get_object_or_none

--- useful/tests/test_helpers.py
+++ useful/tests/test_helpers.py
@@ -12,3 +12,17 @@ class GetObjectOrNoneTestCase(TestCase):
     def test_get_object(self):
         user = get_object_or_none(User, username='test')
         self.assertIsNotNone(user)
+        self.assertEquals(type(user), type(self.user))
+        self.assertEquals(user, self.user)
+
+        user = get_object_or_none(User, email='test@example.com')
+        self.assertIsNotNone(user)
+        self.assertEquals(type(user), type(self.user))
+        self.assertEquals(user, self.user)
+
+    def test_get_none(self):
+        user = get_object_or_none(User, username='nonexistent')
+        self.assertIsNone(user)
+
+        user = get_object_or_none(User, email='doesnot@exist.com')
+        self.assertIsNone(user)

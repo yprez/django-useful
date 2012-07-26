0ff00a8c421606ec385b9d40969bb50b380f48db
Yuri Prezument <y@yprez.com>
2012-07-26 23:37:42 +0300

    Remove logging from test project settings, in order to resolve Django1.3 failures

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -56,26 +56,3 @@ INSTALLED_APPS = (
     'useful'  # Import the app to run tests
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}

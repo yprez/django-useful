22965fc15e3ad9c6d02cfb3c0185ad74f90e8e40
Yuri Prezument <y@yprez.com>
2012-07-27 21:13:04 +0300

    Fix Django1.3 tests

--- useful/tests/test_urls.py
+++ useful/tests/test_urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url, patterns
+try:
+    from django.conf.urls import url, patterns  # Django1.4
+except ImportError:
+    from django.conf.urls.defaults import url, patterns  # Django1.3
 
 from useful.views import ExtraContextTemplateView
+
 
 urlpatterns = patterns('',
     url(r'^test_extra_context_view_no_context$',

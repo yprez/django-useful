fe49bd32159ba2730fcc3c99c33d0bd49770a46f
Yuri Prezument <y@yprez.com>
2012-07-28 18:08:17 +0300

    Add docstrings

--- useful/helpers/json_response.py
+++ useful/helpers/json_response.py
@@ -7,12 +7,21 @@ except:
 
 
 def json_response(data, status=200, serializer=None):
+    """
+    Returns an HttpResponse object containing JSON serialized data
+    The mime-type is set to application/json, and the charset to UTF-8
+    """
     return HttpResponse(json.dumps(data, default=serializer),
                         status=status,
                         content_type='application/json; charset=UTF-8')
 
 
 def jsonp_response(data, callback="f", status=200, serializer=None):
+    """
+    Returns an HttpResponse object containing JSON serialized data,
+    wrapped in a JSONP callback.
+    The mime-type is set to application/x-javascript, and the charset to UTF-8
+    """
     val = json.dumps(data, default=serializer)
     ret = "{callback}('{val}');".format(callback=callback, val=val)
 

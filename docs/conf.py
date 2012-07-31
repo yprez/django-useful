95cdfb6f0ba2dd85a299c5e198699a21699e90f8
Yuri Prezument <y@yprez.com>
2012-07-31 20:38:27 +0300

    Docs conf - fix django settings module for autodoc imports

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../test_project'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -240,3 +241,6 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
+
